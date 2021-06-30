29244b510b327f99517c01d07c81c828136c4b04
m1raynee <pechenko-7@ya.ru>
2021-06-30 17:09:22 +0300

    triggers improvement

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -1,3 +1,5 @@
+import re
+
 import discord
 from discord.ext import commands
 
@@ -7,12 +9,16 @@ from utils import layout
 class Triggers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.regex = re.compile('<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>')
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-
         content = message.content
+        
+        if not message.author.premium_since:
+            if re.match(self.regex, content):
+                return await self.send_webhook(message, content, 'Анимированный бейдж')
+
         if 'TADA' in content:
             await message.add_reaction('🎉')
         if 'LD' in content:
@@ -21,6 +27,15 @@ class Triggers(commands.Cog):
         if '<3' in content:
             await message.add_reaction('♥')
     
+    async def send_webhook(self, message, content, reason=None):
+        wh = await message.channel.create_webhook(
+            name = message.author.display_name, 
+            avatar = await message.author.avatar.read(),
+            reason = reason
+        )
+        await message.delete(reason = reason)
+        await wh.send(content)
+        await wh.delete(reason = reason)
     
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -30,16 +45,7 @@ class Triggers(commands.Cog):
         if msg.author != user:
             return
 
-        rsn = 'Исправление раскладки'
-        wh = await msg.channel.create_webhook(
-            name = user.display_name, 
-            avatar = await user.avatar.url.read(),
-            reason = rsn
-        )
-
-        await msg.delete()
-        await wh.send(f'{msg.content.translate(layout)}\n> **Исправлена раскладка клавиатуры**')
-        await wh.delete()
+        await self.send_webhook(msg, f'{msg.content.translate(layout)}\n> **Исправлена раскладка клавиатуры**', 'Исправление раскладки')
 
 
 def setup(bot):
