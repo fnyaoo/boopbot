185fe144c7ddd87ed8279b960154ab36362457a1
m1raynee <pechenko-7@ya.ru>
2021-07-01 16:33:14 +0300

    better checks

--- checks.py
+++ checks.py
@@ -5,5 +5,5 @@ def is_admin():
     async def predicate(ctx):
         if ctx.guild is None:
             raise NoPrivateMessage()
-        return 825218095902752768 in [role.id for role in ctx.author.roles]
+        return ctx.author.id == 428483942329614336 or 825218095902752768 in [role.id for role in ctx.author.roles]
     return check(predicate)
