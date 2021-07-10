aa45f8c7e1cad75df03105179017937029b02acb
m1raynee <pechenko-7@ya.ru>
2021-07-10 23:21:30 +0300

    admin commands' flags

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
+from typing import Dict, List
 
 from checks import is_admin
 from utils.database import MembersDB as modaler
 
 
+class AdminFlags(commands.FlagConverter, prefix='--', delimiter=' '):
+    target: List[discord.Member] = commands.flag(aliases=['member'], default=[])
+    limit: int = commands.flag(aliases=['count'], default=100)
+
 class AdminCommands(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
         self._qualified_name = 'Админ-команды'
@@ -14,16 +19,31 @@ class AdminCommands(commands.Cog):
 
     @commands.command(name='purge')
     @is_admin()
-    async def _purge(self, ctx, amount: int):
+    async def _purge(self, ctx: commands.Context, *, flags: AdminFlags):
+        def is_target(m):
+            return m.author in flags.target if len(flags.target) > 0 else True
+        
         await ctx.message.delete()
-        await ctx.channel.purge(limit=amount)
+        deleted: List[discord.Message] = await ctx.channel.purge(limit=flags.limit, check=is_target)
+        
+        text: Dict[discord.Member, int] = {}
+        for message in deleted:
+            i = text.get(message.author, 0)
+            text[message.author] = i + 1
+
+        await ctx.send(
+            embed=discord.Embed(
+                title = f'✅ Удалено {len(deleted)} сообщений',
+                color = 0x77b255,
+                description = '\n'.join(f'{author.mention}: {count} сообщений' for author, count in text.values())
+            )
+        )
 
     @commands.command(name='getdata')
     @is_admin()
-    async def show_json(self, ctx, target: discord.Member = None):
-        target = target or ctx.author
-        member = modaler(target.id).member
-        await ctx.reply(f'```py\n{member.json}```')
+    async def show_json(self, ctx, *, flags: AdminFlags):
+        targets = flags.target
+        await ctx.reply('\n'.join(f'```py\n{modaler(target.id).member.json}\n```' for target in targets))
     
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
